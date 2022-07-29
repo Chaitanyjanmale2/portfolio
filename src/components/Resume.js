ff0c3b39e9c05e1f7edb42f2e3ca01b2b19405c8
chaitany <chaitanyjanmale@gmail.com>
2022-07-30 00:06:18 +0530

    updated resume upload

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -6,34 +6,34 @@ import Resumedd from "./_Resumecjup.pdf"
 
 
 
-const Resume =()=>{
- 
-    
-return(
-    <div className="row mt-5 text-center">
-        
-        <div className="container  ">
-     <div className=" col " style={{height:"400px"}}>
-         <div  style={{height:"350px"}}> <iframe className="embed-responsive-item" width="100%" height="100%"  src="https://drive.google.com/file/d/1BupRP_OLDBJIUpZaBPGCmeT9ObPd5Vxn/preview" allowFullScreen allow="autoplay">
+const Resume = () => {
 
-         </iframe></div>
-          
-      
+
+    return (
+        <div className="row mt-5 text-center">
+
+            <div className="container  ">
+                <div className=" col " style={{ height: "400px" }}>
+                    <div style={{ height: "350px" }}> <iframe className="embed-responsive-item" width="100%" height="100%" src="https://drive.google.com/file/d/1Yyw7dPu-hV1S0igYcJT25MBq7AO_67GY/view?usp=sharing" allowFullScreen allow="autoplay">
+
+                    </iframe></div>
 
 
 
-     <div className="m-4"><a href={Resumedd} download className="btn btn-primary">Download</a></div>
-     </div>
-  
-     
-     <Link to="/" className="btn  mt-4 btn-warning ">Go Back To Home</Link>
 
-        
 
+                    <div className="m-4"><a href={Resumedd} download className="btn btn-primary">Download</a></div>
+                </div>
+
+
+                <Link to="/" className="btn  mt-4 btn-warning ">Go Back To Home</Link>
+
+
+
+            </div>
         </div>
-    </div>
-    
-        
-)
+
+
+    )
 }
 export default (Resume)
